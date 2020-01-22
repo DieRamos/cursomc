4bffa6c73685c0e440234eadd0e1ff3853a52fee
Diego Ramos <diegopsr2010@gmail.com>
2020-01-22 09:59:40 -0300

    Retornando ao curso

--- src/main/java/com/diegoramos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diegoramos/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.diegoramos.cursomc.resources;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.diegoramos.cursomc.domain.Categoria;
+import com.diegoramos.cursomc.services.CategoriaService;
+
+import java.util.List;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!!!";
+
+	@Autowired
+	private CategoriaService service;
+
+	@GetMapping(value = "/(id)")
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/diegoramos/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.diegoramos.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegoramos.cursomc.domain.Categoria;
+import com.diegoramos.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
